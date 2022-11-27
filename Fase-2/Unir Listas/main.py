767ff1030670ace054ee32a4d6e607cf6c506e67
Richard Neri <richardmatq@gmail.com>
2022-11-27 17:24:47 -0300

    Update main.py

--- Fase-2/Unir Listas/main.py
+++ Fase-2/Unir Listas/main.py
@@ -1,20 +1,22 @@
+from itertools import zip_longest
+
 capitais = ['Salvador', 'Ubatuba', 'Belo Horizonte', 'Rio de Janeiro', 'Sergipe']
 estados = ['BA', 'SP', 'MG', 'RJ']
 
 def FactorFunc(func):
     def inerFunc(*args, **kwargs):
-        x = len(args[0])
-        y = len(args[1])
-        if x > y:
-            return func(*args, **kwargs, z=y)
-        elif x < y:
-            return func(*args, **kwargs, z=x)
+        x = min(len(args[0]), len(args[1]))
         return func(*args, **kwargs, z=x)
     return inerFunc
 
-@FactorFunc
+
 def zipper(cap, est, z=0):
-    n = [(cap[i], est[i]) for i in range(z)]
+    x = min(len(cap), len(est))
+    n = [(cap[i], est[i]) for i in range(x)]
     return n
+
+print([(x, y) for x, y in zip(capitais, estados)])
+
+print([(x, y) for x, y in zip_longest(capitais, estados, fillvalue='Deconhecida')])
 
 print(zipper(capitais, estados))
