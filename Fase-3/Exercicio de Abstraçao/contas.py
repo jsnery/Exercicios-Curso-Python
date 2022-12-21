b208a17c964a7d7a20243ef85d1cddf06f1e1c03
Richard Neri <richardmatq@gmail.com>
2022-12-21 12:41:10 -0300

    Update contas.py

--- Fase-3/Exercicio de Abstraçao/contas.py
+++ Fase-3/Exercicio de Abstraçao/contas.py
@@ -7,7 +7,7 @@ class Conta(ABC):
         self.numero = numero
         self.saldo = 0.0
 
-    def depositar(self, value: float) -> bool:
+    def _depositar(self, value: float) -> bool:
         if value <= 0:
             return False
 
@@ -15,37 +15,38 @@ class Conta(ABC):
         return True
 
     @abstractmethod
-    def sacar(self, value: float) -> bool: ...
+    def _sacar(self, value: float) -> bool: ...
 
     def __repr__(self) -> str:
         class_name = type(self).__name__
-        attrs = f'{self.agencia!r}, {self.numero!r}'
+        attrs = f'Agência: {self.agencia!r},'\
+                f' Conta: {self.numero!r}, Saldo: {self.saldo!r}'
         return f'{class_name}({attrs})'
 
 
 class ContaCorrente(Conta):
-    def __init__(self, numero: int, agencia: int, limite: float = 0) -> None:
+    def __init__(self, numero: int, agencia: int, limite: int | float = 0):
         super().__init__(numero, agencia)
         if limite > 0:
             self._limite = -limite
         else:
             self._limite = 0
 
-    def sacar(self, value: float):
+    def _sacar(self, value: int | float):
         if (self.saldo - value) < self._limite:
             return False
 
         self.saldo -= value
         return True
 
-    def set_limite(self, value: float) -> None:
+    def set_limite(self, value: int | float) -> None:
         if value > 0:
             self._limite = -value
 
 
 class ContaPoupanca(Conta):
 
-    def sacar(self, value: float):
+    def _sacar(self, value: float):
         if value > self.saldo or value <= 0:
             return False
 
